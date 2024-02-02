24fba1e73014c82f3ed0da7f45c3229dbdb4343f
mirbyte <83219244+mirbyte@users.noreply.github.com>
2024-02-02 14:21:54 +0200

    Update Frontpage Autoplay Off.js

--- Frontpage Autoplay Off.js
+++ Frontpage Autoplay Off.js
@@ -22,10 +22,10 @@
         fuckoffautoplay();
     });
     // watch for changes in the DOM
-    var observerFuckOffAutoplay = new MutationObserver(function(mutations) {
+    var observer = new MutationObserver(function(mutations) {
         mutations.forEach(function(mutation) {
             fuckoffautoplay();
         });
     });
-    observerFuckOffAutoplay.observe(document.body, { subtree: true, childList: true });
+    observer.observe(document.body, { subtree: true, childList: true });
 })();
