4e75517f335ffe8efea54ea3385dffd9c856d94e
Mirko <83219244+mirbyte@users.noreply.github.com>
2025-01-25 18:48:23 +0200

    Add files via upload

---
+++ Prime Notifications Remover.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name        Twitch.tv Amazon Prime Ad Remover
+// @namespace   Violentmonkey Scripts
+// @match       https://www.twitch.tv/*
+// @grant       none
+// @version     1.1
+// @author      mirbyte (https://github.com/mirbyte)
+// @description Removes amazon prime ad/button on twitch.tv. Check GitHub page for the demonstration image.
+// @icon         https://banner2.cleanpng.com/20180513/xie/kisspng-twitch-computer-icons-logo-5af8037d689af0.0981376915262032614285.jpg
+// ==/UserScript==
+
+
+(function() {
+    'use strict';
+    function removePrime() {
+        var primeOffersDiv = document.querySelector('.Layout-sc-1xcs6mc-0.cwtKyw.prime-offers');
+        if (primeOffersDiv) {
+            primeOffersDiv.remove();
+        }
+    }
+    document.addEventListener('DOMContentLoaded', removePrime);
+    // watch for changes in the DOM
+    var observer = new MutationObserver(function(mutations) {
+        mutations.forEach(function(mutation) {
+            removePrime();
+        });
+    });
+    observer.observe(document.body, { subtree: true, childList: true });
+})();
