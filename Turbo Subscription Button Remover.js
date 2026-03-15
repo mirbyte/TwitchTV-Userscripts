5f25cf188cb565e023f0664dafd5ba0e43d93a86
mirko <83219244+mirbyte@users.noreply.github.com>
2026-03-16 00:07:13 +0200

    Update Turbo Subscription Button Remover.js

--- Turbo Subscription Button Remover.js
+++ Turbo Subscription Button Remover.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name        Turbo Subscription Button Remover
-// @namespace   https://github.com/mirbyte/TwitchTV-Userscripts/edit/main/Turbo%20Subscription%20Button%20Remover.js
+// @namespace   https://github.com/mirbyte/TwitchTV-Userscripts
 // @match       *://www.twitch.tv/*
 // @grant       none
-// @version     2.1
+// @version     2.2
 // @author      mirbyte
 // @description Edits the Turbo ad banner to be a transparent placeholder. Check GitHub page for the demonstration image.
-// @icon        https://banner2.cleanpng.com/20180513/xie/kisspng-twitch-computer-icons-logo-5af8037d689af0.0981376915262032614285.jpg
+// @icon        https://www.twitch.tv/favicon.ico
 // @downloadURL https://update.greasyfork.org/scripts/524848/Turbo%20Subscription%20Button%20Remover.user.js
 // @updateURL https://update.greasyfork.org/scripts/524848/Turbo%20Subscription%20Button%20Remover.meta.js
 // ==/UserScript==
