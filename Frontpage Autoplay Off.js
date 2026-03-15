3f2b6a81080b3f4a1c032fa0d9eb80f40012fef2
mirko <83219244+mirbyte@users.noreply.github.com>
2026-03-16 00:07:56 +0200

    Update Frontpage Autoplay Off.js

--- Frontpage Autoplay Off.js
+++ Frontpage Autoplay Off.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name        Twitch.tv Frontpage Autoplay Off
-// @namespace   https://github.com/mirbyte/TwitchTV-Userscripts/edit/main/Frontpage%20Autoplay%20Off.js
+// @namespace   https://github.com/mirbyte/TwitchTV-Userscripts
 // @match       https://www.twitch.tv/
 // @grant       none
-// @version     1.3
+// @version     1.4
 // @author      mirbyte
 // @description Pauses Twitch.tv frontpage carousel video that autoplays recommended streams. Check GitHub page for the demonstration image.
-// @icon        https://banner2.cleanpng.com/20180513/xie/kisspng-twitch-computer-icons-logo-5af8037d689af0.0981376915262032614285.jpg
+// @icon        https://www.twitch.tv/favicon.ico
 // ==/UserScript==
 
 
