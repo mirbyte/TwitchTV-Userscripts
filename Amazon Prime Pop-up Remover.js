fd5419ea8bd95f57aaaa2ab3a377c76e6a40a207
mirko <83219244+mirbyte@users.noreply.github.com>
2026-03-16 00:08:11 +0200

    Update Amazon Prime Pop-up Remover.js

--- Amazon Prime Pop-up Remover.js
+++ Amazon Prime Pop-up Remover.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name        Twitch.tv Prime Pop-up Remover
-// @namespace   https://github.com/mirbyte/TwitchTV-Userscripts/edit/main/Amazon%20Prime%20Pop-up%20Remover.js
+// @namespace   https://github.com/mirbyte/TwitchTV-Userscripts
 // @match       https://www.twitch.tv/*
 // @grant       none
-// @version     1.3
+// @version     1.4
 // @author      mirbyte
 // @description Removes Amazon Prime overlay ad on Twitch streams. Check GitHub page for the demonstration image.
-// @icon        https://banner2.cleanpng.com/20180513/xie/kisspng-twitch-computer-icons-logo-5af8037d689af0.0981376915262032614285.jpg
+// @icon        https://www.twitch.tv/favicon.ico
 // ==/UserScript==
 
 (function () {
